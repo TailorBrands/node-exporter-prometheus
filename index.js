2c7b40751cd35b2e982b3009a7d5bf84f421ef91
liorrozen <liorr@tailorbrands.com>
2020-03-16 11:28:41 +0200

    Lint passes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const DEFAULT_APP_NAME = 'node';
  * @param {object} opts An object containing the required configuration.
  *    appName                           - The name to be used as a default label
  *    ignoredRoutes (optional)          - An array of routes that should be ignored
- *    collectDefaultMetrics (optional)  - A boolean indicating whether or not to collect default metrics (default: true)
+ *    collectDefaultMetrics (optional)  - A boolean indicating whether or not to collect default metrics (default: false)
  * @returns {object} The internal prometheus client, middleware function, and metrics function.
  */
 module.exports = ({appName = DEFAULT_APP_NAME, ignoredRoutes = DEFAULT_IGNORED_ROUTES, collectDefaultMetrics = false}) => {
@@ -16,40 +16,41 @@ module.exports = ({appName = DEFAULT_APP_NAME, ignoredRoutes = DEFAULT_IGNORED_R
 
 	const durationSummary = createDurationSummary();
 	const statusCounter = createStatusCounter();
-    let defaultMetricsInterval = null;
+	let defaultMetricsInterval = null; // eslint-disable-line no-unused-vars
 
-    if (collectDefaultMetrics) {
-        defaultMetricsInterval = Prometheus.collectDefaultMetrics({timeout: DEFAULT_TIMEOUT_MS});
-    }
+	if (collectDefaultMetrics) {
+		defaultMetricsInterval = Prometheus.collectDefaultMetrics({timeout: DEFAULT_TIMEOUT_MS});
+	}
 
-    /**
+	/**
      * This is the middleware function that actually measures response metrics. For every request the
      * duration is measured as well as the request path, request method, and response status.
      * @param {Request} req A request object
-     * @param {Response} res A response object
+     * @param {Response} response A response object
      * @param {Function} next Function to trigger the next middleware function in the chain
      */
-	const middlewareFunc = (req, res, next) => {
-		if (!ignoredRoutes.includes(req.path)) {
+	const middlewareFunc = (request, response, next) => {
+		if (!ignoredRoutes.includes(request.path)) {
 			const end = durationSummary.startTimer();
-			res.on('finish', () => {
-				const reqData = {status: res.statusCode};
-				statusCounter.inc(reqData, 1); // Observes the status code of the request
+			response.on('finish', () => {
+				const requestData = {status: response.statusCode};
+				statusCounter.inc(requestData, 1); // Observes the status code of the request
 				end(); // Observes the duration of the request
 			});
 		}
+
 		next();
 	};
 
-    /**
+	/**
      * This is the function that serves the metrics data - use it as follows:
      * `app.get('/metrics', PrometheusExporter.metrics);`
      * @param {Request} req A request object
-     * @param {Response} res A response object
+     * @param {Response} response A response object
      */
-	const metricsFunc = (req, res) => {
-		res.set('Content-Type', Prometheus.register.contentType);
-		res.end(Prometheus.register.metrics());
+	const metricsFunc = (request, response) => {
+		response.set('Content-Type', Prometheus.register.contentType);
+		response.end(Prometheus.register.metrics());
 	};
 
 	return {
@@ -83,6 +84,6 @@ function createStatusCounter() {
 	return new Prometheus.Counter({
 		name: 'node_http_requests_total',
 		help: 'Response status codes',
-        labelNames: ['status']
+		labelNames: ['status']
 	});
 }
